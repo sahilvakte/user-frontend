fe889b664323a5f4209578434e90036ec2cee52f
Sahil <sahilvaktepatil123@gmail.com>
2022-08-05 17:09:51 +0530

    Login UI with Backend Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/Signup/Signup';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div>
-      <Signup/>
+      {/* <Signup/> */}
+      <Login/>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../Login/Login.css";
+
+import { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(`http://localhost:5000/api/login`, {
+        username: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  return (
+    <div>
+      <div className="user-login-container">
+        <div className="user-login-card">
+          <div>
+            <label>Email or Phone</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button onClick={login}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
