0374f5b6be7dce484306437ce475c16a458b2145
Sahil <sahilvaktepatil123@gmail.com>
2022-08-20 10:48:14 +0530

    Data is Fetched from Db to UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Signup from './Components/Signup/Signup';
-import Login from './Components/Login/Login';
-import Addproduct from './Components/Addproduct/Addproduct';
+import logo from "./logo.svg";
+import "./App.css";
+import Signup from "./Components/Signup/Signup";
+import Login from "./Components/Login/Login";
+import Addproduct from "./Components/Addproduct/Addproduct";
+import FetchAPI from "./Components/Showproduct/FetchAPI";
+
 
 function App() {
+
+
   return (
     <div>
       {/* <Signup/> */}
       {/* <Login/> */}
-      <Addproduct/>
+      {/* <Addproduct/> */}
+      {/* <Showproduct  /> */}
+      <FetchAPI/>
     </div>
   );
 }
---
+++ src/Components/Showproduct/FetchAPI.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+function FetchAPI() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/prodapi/showproduct").then((result) => {
+      result.json().then((resp) => {
+        
+        console.warn("result",resp.response)
+        setData(resp.response);
+      });
+    });
+  }, []);
+  // console.warn(data);
+
+  return (
+    <div>
+      <h1>Hellllo</h1>
+      <table>
+        <tr>
+          <td>Name</td>
+          <td>Description</td>
+          <td>Category</td>
+          <td>Price</td>
+        </tr>
+        {data.map((item) => 
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.desc}</td>
+            <td>{item.category}</td>
+            <td>{item.price}</td>
+          </tr>
+        )}
+      </table>
+    </div>
+  );
+}
+
+export default FetchAPI;
