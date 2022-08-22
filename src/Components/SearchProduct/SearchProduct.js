698206d364e527f000fd4fb7631eec5fc07b359c
Sahil <sahilvaktepatil123@gmail.com>
2022-08-22 11:51:08 +0530

    Quantity of Product Update

--- src/Components/SearchProduct/SearchProduct.js
+++ src/Components/SearchProduct/SearchProduct.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../SearchProduct/SearchProduct.css";
 
 function SearchProduct() {
   const [loading, setLoading] = useState(false);
@@ -27,6 +28,28 @@ function SearchProduct() {
     } else {
       return setOrderproduct([...orderproduct, product]);
     }
+  };
+
+  // console.log("orderproduct", orderproduct);
+
+  const onIncrement = (item, index) => {
+    // console.log("index", index);
+    let newArr = [...orderproduct];
+    newArr[index].qty = newArr[index].qty
+      ? newArr[index].qty + 1
+      : 2;
+    setOrderproduct(newArr);
+    // console.log("orderproduct", newArr);
+  };
+
+  const onDecrement = (item, index) => {
+    // console.log("index", index);
+    let newArr = [...orderproduct];
+    newArr[index].qty = newArr[index].qty
+      ? newArr[index].qty - 1
+      : 0;
+    setOrderproduct(newArr);
+    // console.log("orderproduct", newArr);
   };
 
   return (
@@ -68,13 +91,22 @@ function SearchProduct() {
         Selected Product For Order
         {orderproduct &&
           orderproduct.length > 0 &&
-          orderproduct.map((item) => (
+          orderproduct.map((item, index) => (
             <h5 key={item.id}>
               Name :{item.name}
               <br />
               Price : {item.price}
               <br /> Description : {item.desc}
               <br /> Category :{item.category}
+              <div className="quantity-controlling">
+                <div>
+                  <button onClick={() => onDecrement(item, index)}>-</button>
+                </div>
+                {item.qty ? item.qty : 1}
+                <div>
+                  <button onClick={() => onIncrement(item, index)}>+</button>
+                </div>
+              </div>
             </h5>
           ))}
       </div>
